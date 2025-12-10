0002308b3b9243308476d0e6e6c7d9c13cfb2ec4
Maksim Zhigarev <maks825@mail.ru>
2025-12-10 19:58:39 +0300

    Selenide: убраны лишние зависимости allure. Test Containers: добавлено выполнение sql

--- core/src/main/java/ru/origami/testit_allure/utils/BeforeAllTestItAllureExtension.java
+++ core/src/main/java/ru/origami/testit_allure/utils/BeforeAllTestItAllureExtension.java
@@ -15,7 +15,6 @@ import javassist.bytecode.ClassFile;
 import javassist.bytecode.ConstPool;
 import javassist.bytecode.annotation.StringMemberValue;
 import lombok.extern.slf4j.Slf4j;
-//import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
