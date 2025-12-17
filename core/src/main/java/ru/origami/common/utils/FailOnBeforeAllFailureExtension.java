9228e0c3db3c717d8147a89b7b0c407bed2a9509
Maksim Zhigarev <maks825@mail.ru>
2025-12-17 19:24:37 +0300

    Test Containers: добавлены методы для создания IBM MQ контейнера. IBM MQ: доработан метод обработки Message. Core: доработан механизм работы с конфигурациями. Fail BeforeAll

--- core/src/main/java/ru/origami/common/utils/FailOnBeforeAllFailureExtension.java
+++ core/src/main/java/ru/origami/common/utils/FailOnBeforeAllFailureExtension.java
@@ -1,6 +1,8 @@
 package ru.origami.common.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.InvocationInterceptor;
@@ -8,7 +10,11 @@ import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
 import ru.origami.common.models.BeforeAllErrorInfo;
 
 import java.lang.reflect.Method;
+import java.util.Objects;
 
+import static ru.origami.common.environment.Language.getLangValue;
+
+@Slf4j
 public class FailOnBeforeAllFailureExtension implements BeforeEachCallback, InvocationInterceptor {
 
     private static final String INIT_ERROR_KEY = "beforeAllError";
@@ -25,21 +31,25 @@ public class FailOnBeforeAllFailureExtension implements BeforeEachCallback, Invo
     @Override
     public void interceptBeforeAllMethod(Invocation<Void> invocation, ReflectiveInvocationContext<Method> invocationContext,
                                          ExtensionContext extensionContext) {
-        Method beforeAllMethod = invocationContext.getExecutable();
-        String className = beforeAllMethod.getDeclaringClass().getName();
-        String methodName = beforeAllMethod.getName();
-
         try {
             invocation.proceed();
         } catch (Throwable t) {
-            System.err.printf("BeforeAll FAILED in %s#%s: %s%n", className, methodName, t);
+            Method beforeAllMethod = invocationContext.getExecutable();
+            String className = beforeAllMethod.getDeclaringClass().getName();
+            String methodName = beforeAllMethod.getName();
 
+            DisplayName displayNameAnn = beforeAllMethod.getAnnotation(DisplayName.class);
+            String methodDisplayName = (displayNameAnn != null)
+                    ? displayNameAnn.value()
+                    : methodName;
+
+            log.error(getLangValue("before.all.error"), className, methodName, t);
             t.printStackTrace(System.err);
 
             BeforeAllErrorInfo info = BeforeAllErrorInfo.Builder()
                     .setThrowable(t)
                     .setTestClassName(className)
-                    .setMethodName(methodName)
+                    .setMethodName(methodDisplayName)
                     .build();
             getStore(extensionContext).put(INIT_ERROR_KEY, info);
         }
@@ -49,13 +59,8 @@ public class FailOnBeforeAllFailureExtension implements BeforeEachCallback, Invo
     public void beforeEach(ExtensionContext context) {
         BeforeAllErrorInfo info = getStore(context).get(INIT_ERROR_KEY, BeforeAllErrorInfo.class);
 
-        if (info != null) {
-            String message = String.format(
-                    "Инициализация в @BeforeAll упала в %s#%s: %s",
-                    info.getTestClassName(),
-                    info.getMethodName(),
-                    info.getThrowable().toString()
-            );
+        if (Objects.nonNull(info)) {
+            String message = getLangValue("before.all.error.after.each").formatted(info.getMethodName(), info.getThrowable().toString());
 
             Assertions.fail(message, info.getThrowable());
         }
